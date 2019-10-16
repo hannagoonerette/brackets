4ce95609aa7897b5fb816f1d9a5bb1da6a653965
Hanna <hannagoonerette@gmail.com>
2019-10-16 11:11:09 +0300

    feat: add part of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = bracketsConfig[0][0];
+  let b = bracketsConfig[0][1];
+  let strArr = str.split('');
+  let countA = 0;
+  let countB = 0;
+  for (let item of strArr){
+      if (item == a){
+        countA++;
+      };
+      if (item == b){
+        countB++;
+      }
+  }
+  if (countA == countB){
+      return true;
+  }
+  else{
+      return false;
+  }
 }
