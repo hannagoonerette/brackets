967c8ea48b4ba3b1d84f0e898edd9134b0232c29
Hanna <hannagoonerette@gmail.com>
2019-10-16 14:21:57 +0300

    fix: change algoritm

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  let a = bracketsConfig[0][0];
-  let b = bracketsConfig[0][1];
-  let strArr = str.split('');
-  let countA = 0;
-  let countB = 0;
-  for (let item of strArr){
-      if (item == a){
-        countA++;
-      };
-      if (item == b){
-        countB++;
+    let strArr = str.split('');
+    let count = 0;
+    let countA = 0;
+    let countB = 0;
+      for (let item of strArr){
+          for (let i = 0; i<bracketsConfig.length; i++){
+              let a = bracketsConfig[i][0];
+              let b = bracketsConfig[i][1];
+              if (a == b && item == a){
+                  count++;
+              }
+              else{
+                  if (item == a){
+                      countA++;
+                  }
+                  if (item == b){
+                      countB++;
+                  }
+              }
+          }
       }
-  }
-  if (countA == countB){
-      return true;
-  }
-  else{
-      return false;
-  }
+      if (count%2 !=0 || countA%2 != 0 || countB%2 != 0){
+          return false;
+      }
+      else{
+          return true;
+      }
 }
